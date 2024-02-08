4001bf4d785c97b890cc824f48af89115ce697b1
추세빈 <cntpqls0218@gmail.com>
2024-02-08 23:52:35 +0900

    [build] 프로젝트 환경 세팅

---
+++ src/test/java/server/nengcipe/NengcipeApplicationTests.java
@@ -0,0 +1,13 @@
+package server.nengcipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NengcipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
